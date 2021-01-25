970e631b35a2f5b1794d0a1d3d44e725d2c7aee6
Victor Ortiz <victor@MacBook-Pro-3.local>
2021-01-25 12:45:30 -0600

    fix placeholder

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,7 @@ import styles from "./Search.module.scss"
 function Card(props) {
     return (
        <div className={styles.searchBar}>
-            <input type="text" onChange={props.search} placeholder="wubba lubba dub-dub"/>
+            <input type="text" onChange={props.search} placeholder="Buscar personaje..."/>
        </div>
     )
 }
